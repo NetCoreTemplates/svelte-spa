05a62d0a970a8b5f919d0b77f91100c2ddb71e62
Demis Bellot <demis.bellot@gmail.com>
2020-05-28 15:47:14 +0800

    Upgrade to latest svelte/deps template

--- MyApp/rollup.config.js
+++ MyApp/rollup.config.js
@@ -1,11 +1,9 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
-import builtins from 'rollup-plugin-node-builtins';
-import globals from 'rollup-plugin-node-globals';
-
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import json from '@rollup/plugin-json';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -19,25 +17,30 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('wwwroot/bundle.css');
-			}
+				// enable run-time checks when not in production
+				dev: !production,
+				// we'll extract any component CSS out into
+				// a separate file - better for performance
+				css: css => {
+						css.write('public/build/bundle.css');
+				}
 		}),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
+		// some cases you'll need additional configuration -
 		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve({ preferBuiltins: true }),
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+				browser: true,
+				dedupe: ['svelte']
+		}),
 		commonjs(),
 		json(),
-		globals(),
-		builtins(),
+
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('public'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
